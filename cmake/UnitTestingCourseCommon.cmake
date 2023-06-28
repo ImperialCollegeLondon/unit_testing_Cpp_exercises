5f8856d75f6f7600b617614d563088248766e44b
Alex Dewar <a.dewar@imperial.ac.uk>
2023-06-28 12:14:04 +0100

    Enable extra compiler warnings by default

--- cmake/UnitTestingCourseCommon.cmake
+++ cmake/UnitTestingCourseCommon.cmake
@@ -1,9 +1,19 @@
 # Common CMake stuff to be used by different chapters. To make your own CMake project,
 # you will need to have this (or something similar) in your CMakeLists.txt file.
 
+# Stop this file being included more than once
+include_guard(GLOBAL)
+
 # GoogleTest requires at least C++14
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+# Enable compiler warnings and standard conformance
+if(MSVC)
+    add_compile_options(/W4 /permissive-)
+else()
+    add_compile_options(-Wall -Wextra -Wpedantic)
+endif()
 
 enable_testing()
 
