22d0184aba2a315cad789980a9e5f080daa39f96
Alex Dewar <a.dewar@imperial.ac.uk>
2023-07-19 13:11:33 +0100

    Add example from Chapter 1

---
+++ src/Chapter1/program.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+/*Returns the n'th term of the Fibonacci sequence.*/
+int recursive_fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2);
+    }
+}
+
+int main()
+{
+    // Manual tests
+    // Case 1: When n is 0 or 1
+    int n = 0;
+    std::cout << "Fib(" << n << ") = " << recursive_fibonacci(n) << "\n";
+
+    n = 1;
+    std::cout << "Fib(" << n << ") = " << recursive_fibonacci(n) << "\n";
+
+    // Case 2: When n is greater than 1.
+    n = 5;
+    std::cout << "Fib(" << n << ") = " << recursive_fibonacci(n) << "\n";
+
+    // Case 3: When n is negative.
+    n = -3;
+    std::cout << "Fib(" << n << ") = " << recursive_fibonacci(n) << "\n";
+
+    return 0;
+}
