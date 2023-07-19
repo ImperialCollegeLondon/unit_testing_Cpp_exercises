af51c5ec98235edfb9f2f0c800dc702df46ab0c3
Alex Dewar <a.dewar@imperial.ac.uk>
2023-07-19 11:45:10 +0100

    Add code for Chapter 4

---
+++ src/Chapter4/2_test_using_for_loop.cpp
@@ -0,0 +1,21 @@
+#include "employee.h"
+#include <gtest/gtest.h>
+
+// Create a test fixture.
+class EmployeeTestFixture : public ::testing::Test
+{
+  public:
+    Employee employee{"John", 25, 8000, 3, 2000};
+};
+
+// Test net bonus works fine for different number of years.
+TEST_F(EmployeeTestFixture, NetBonusIsCorrectForDifferentYears)
+{
+    auto input = std::vector<int>{5, 15};
+    auto expected_output = std::vector<int>{2000, 3000};
+    for (int i = 0; i < input.size(); i++)
+    {
+        employee.setNumberYearsEmployed(input[i]);
+        EXPECT_EQ(employee.getNetBonus(), expected_output[i]);
+    }
+}
