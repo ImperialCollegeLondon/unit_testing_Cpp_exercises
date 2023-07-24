af1e956143619e3f03da2dafbe9504958c89e776
Alex Dewar <a.dewar@imperial.ac.uk>
2023-07-24 12:12:04 +0100

    Add code for Chapter 5
    I dropped employee_table.cpp because it is unneeded and broken.

---
+++ src/Chapter5/test_dependency_injection.cpp
@@ -0,0 +1,86 @@
+#include "gtest/gtest.h"
+#include <cmath>
+#include <iostream>
+
+/// @brief A "complex" calculation of the norm.
+/// @param array The vector to calculate the norm for.
+/// @return The actual norm of the vector.
+double calculate_norm(const std::vector<double> &array)
+{
+    double output{0};
+    for (int i{0}; i < array.size(); ++i)
+    {
+        output += array[i] * array[i];
+    }
+    return std::sqrt(output);
+}
+
+/// @brief Just a stub, so we can get a simple value to test things with.
+/// @param array The vector to calculate the norm for.
+/// @return The norm, a dummy value in this case.
+double norm_stub(const std::vector<double> &array)
+{
+    return 10.0;
+}
+
+/// @brief We cannot do dependency injection, so the result depends on what calculate_norm is doing
+/// @param array The vector to normalize.
+/// @return The normalized vector.
+std::vector<double> normalize_v1(const std::vector<double> &array)
+{
+    double norm{calculate_norm(array)};
+    std::vector<double> output;
+
+    for (int i{0}; i < array.size(); ++i)
+    {
+        output.push_back(array[i] / norm);
+    }
+    return output;
+}
+
+/// @brief The norm calculation is injected, so we control that value.
+/// @param array The vector to normalize.
+/// @param func The function used to calculate the norm.
+/// @return The normalized vector.
+std::vector<double> normalize_v2(
+    const std::vector<double> &array,
+    std::function<double(const std::vector<double> &)> func = calculate_norm)
+{
+    double norm{func(array)};
+    std::vector<double> output;
+
+    for (int i{0}; i < array.size(); ++i)
+    {
+        output.push_back(array[i] / norm);
+    }
+    return output;
+}
+
+TEST(NormalizeTest, WithoutDependencyInjection)
+{
+    std::vector<double> input{1, 2, 3};
+
+    // We need the exact value of the norm in order to check that
+    // the calculation is correct.
+    double factor{calculate_norm(input)};
+    std::vector<double> output = normalize_v1(input);
+
+    for (int i{0}; i < input.size(); ++i)
+    {
+        EXPECT_EQ(output[i], input[i] / factor);
+    }
+}
+
+TEST(NormalizeTest, WithDependencyInjection)
+{
+    std::vector<double> input{1, 2, 3};
+
+    // Here the exact value of the norm is meaningless as we control it.
+    double factor{norm_stub(input)};
+    std::vector<double> output = normalize_v2(input, norm_stub);
+
+    for (int i{0}; i < input.size(); ++i)
+    {
+        EXPECT_EQ(output[i], input[i] / factor);
+    }
+}
---
+++ src/Chapter5/test_mock_and_spy_virtual.cpp
@@ -0,0 +1,58 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include <iostream>
+
+class Animal
+{
+  public:
+    virtual ~Animal() = default;
+    virtual double walk(int steps) = 0;
+    virtual void eat(double carbs) = 0;
+    virtual void die() = 0;
+};
+
+bool isAliveAtEndOfDay(int steps, double carbs, Animal *animal)
+{
+    double spent_carbs{animal->walk(steps)};
+    if (spent_carbs > carbs)
+    {
+        animal->die();
+        return false;
+    }
+    animal->eat(carbs - spent_carbs);
+    return true;
+};
+
+class MockAnimal : public Animal
+{
+  public:
+    MOCK_METHOD(double, walk, (int), (override));
+    MOCK_METHOD(void, eat, (double), (override));
+    MOCK_METHOD(void, die, (), (override));
+};
+
+TEST(IsAliveTestVirtual, Lives)
+{
+    MockAnimal animal;
+    int steps{400};
+    double carbs{2000.0};
+    double consumed{500.0};
+
+    EXPECT_CALL(animal, walk(steps)).Times(1).WillOnce(::testing::Return(consumed));
+    EXPECT_CALL(animal, eat(carbs - consumed)).Times(1);
+    EXPECT_CALL(animal, die()).Times(0);
+    ASSERT_TRUE(isAliveAtEndOfDay(steps, carbs, &animal));
+};
+
+TEST(IsAliveTestVirtual, Dies)
+{
+    MockAnimal animal;
+    int steps{400};
+    double carbs{2000.0};
+    double consumed{5000.0};
+
+    EXPECT_CALL(animal, walk(steps)).Times(1).WillOnce(::testing::Return(consumed));
+    EXPECT_CALL(animal, eat(carbs - consumed)).Times(0);
+    EXPECT_CALL(animal, die()).Times(1);
+    ASSERT_FALSE(isAliveAtEndOfDay(steps, carbs, &animal));
+};
